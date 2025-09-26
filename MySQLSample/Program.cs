a78d96db576b36066a4b1e9a4fac8fde7c8f869e
Andrew Moscardino <andrew@moscardino.net>
2025-09-26 08:44:29 -0400

    First working version using raw MySQL connection (no EF Core)

---
+++ MySQLSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using Devart.Data.MySql;
+
+var key = File.ReadAllText("devart.key");
+
+var connStringBuilder = new MySqlConnectionStringBuilder
+{
+    Server = "localhost",
+    Database = "grv_dev",
+    UserId = "grv_dev_user",
+    Password = "Welcome1docker",
+    LicenseKey = key
+};
+
+using var connection = new MySqlConnection(connStringBuilder.ConnectionString);
+connection.Open();
+
+using var command = connection.CreateCommand();
+command.CommandText = "select count(*) from store";
+
+var result = command.ExecuteScalar();
+
+Console.WriteLine($"Number of stores: {result}");
